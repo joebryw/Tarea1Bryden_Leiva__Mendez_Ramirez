3c386a1a78a8b40b215ddb3ad32a895e7cf203cb
lleival <lezzinll370@gmail.com>
2020-02-27 11:30:56 -0600

    Errores flake8
    Código que incluye varios errores detectables con flake8, se describe cada uno mediante un comentario.

---
+++ ErroresDetectables_flake8.py
@@ -0,0 +1,14 @@
+# Se importa una librería que no se utiliza en el programa
+import random
+
+# Se deja una sola linea en blanco antes de definir una función
+# Se omite un espacio luego de la coma en los argumentos de la función
+# Y se agrega un espacio entre el nombre de la función y sus argumentos
+
+def operacion (A,B):
+
+# En ambas lineas se omiten espacios al rededos de los operadores
+    Suma=A+B
+    respuesta=Suma
+
+    return respuesta
